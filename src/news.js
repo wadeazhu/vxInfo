40e3c053c614350688791b83c35ecac9f13df6fc
祝奇 <zhuq@meiyibao.com>
2022-01-25 10:54:07 +0800

    修改新闻的内容

--- src/news.js
+++ src/news.js
@@ -5,28 +5,41 @@ import API from './api/msg.js'
 dotenv.config()
 
 // 其他时间发送新闻
+// API.getTopNews().then(({newList}) => {
+//     return newList.map(item => (
+//         {
+//             title: item.Title,
+//             picurl: item.Image.url,
+//             url: item.Url
+//         }
+//     ))
+// }).then(articles => {
+//     let list = []
+//     for (let i = 0 ; i < articles.length; i++) {
+//         if (i % 8 === 0 && i !== 0) {
+//             wxNotify({
+//                 "msgtype" : "news",
+//                 news: {articles: list}
+//             }).then(() => {})
+//             list = []
+//             break
+//         }else {
+//             list.push(articles[i ])
+//         }
+//     }
+// })
+
 API.getTopNews().then(({newList}) => {
-    return newList.map(item => (
-        {
-            title: item.Title,
-            picurl: item.Image.url,
-            url: item.Url
-        }
-    ))
-}).then(articles => {
-    let list = []
-    for (let i = 0 ; i < articles.length; i++) {
-        if (i % 8 === 0 && i !== 0) {
-            wxNotify({
-                "msgtype" : "news",
-                news: {articles: list}
-            }).then(() => {})
-            list = []
-            break
-        }else {
-            list.push(articles[i ])
-        }
-    }
+    let content = `~~~今日头条~\n\n\n`
+    newList.forEach((item,index) => {
+        content += `${index+1}、${item.Title}\n\n`
+    })
+    return content
+}).then(content => {
+    wxNotify({
+        msgtype : "text",
+        text: {content}
+    }).then(() => {})
 })
 
 
